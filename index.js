643feb83b78fb054ed525f52b322d2f646f7edda
EuropePUSH <europesnusrovelt@gmail.com>
2025-11-11 14:59:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -29,6 +29,9 @@ app.use(cors());
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 
+//------ Tiktok url verifikation --------------
+app.use(express.static("./"));
+
 // ----------------- SUPABASE -----------------
 const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_KEY);
 
